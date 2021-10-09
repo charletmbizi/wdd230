149cd2f61cac1a9918952e6174afff67d3a11f62
charletmbizi <90606039+charletmbizi@users.noreply.github.com>
2021-10-09 16:40:25 +0200

    Update lesson3.js

--- lesson3/js/lesson3.js
+++ lesson3/js/lesson3.js
@@ -1,4 +1,4 @@
 document.getElementById("lastmodified").innerHTML = document.lastModified;
 function date() {
-  document.getElementById("year").innerHTML = new Date().getFullYear()
+  document.getElementById("year").innerHTML = year;
 }
