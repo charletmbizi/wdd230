311f895855f0f3d04702aead8d8f3edc36b1397c
charletmbizi <90606039+charletmbizi@users.noreply.github.com>
2021-11-04 12:10:52 +0200

    Update windchill.js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,1 +1,19 @@
-
+fuction buildWC(speed, temp) {
+  let windchill = document.getElementByID('windchill');
+  
+  // Compute windchill
+  let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow (speed, 0.16) + 0.4275 * temp * Math.pow (speed, 0.16);
+  console.log(wc);
+  
+  //Round the answer down to an integer
+  wc = Math.floor(wc);
+  
+  //If chill is greater than temp, return the temp
+  wc = (wc > temp) ? temp : wc;
+  
+  //Display the wiindchill
+  console.log(wc);
+  
+  //Feels like '+wc+' F
+  feelTemp.innerHTML = WC;
+}
