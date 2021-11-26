41f17a4215f76df503b8c032adb6d3e0158de0f7
charletmbizi <90606039+charletmbizi@users.noreply.github.com>
2021-11-26 10:03:26 +0200

    Create windchill.js

---
+++ lesson-11/js/windchill.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+// Use the windchill function
+let speed = 25;
+let temp = 10;
+let windchill = document.getElementById('windchill');
+windchill.innerHTML = buildWC(speed, temp);
+});
+
+function buildWC(speed, temp) {
+  // let windchill = document.getElementByID('windchill');
+  
+  // Compute windchill
+  let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow (speed, 0.16) + 0.4275 * temp * Math.pow (speed, 0.16);
+  console.log(wc);
+  
+  //Round the answer down to an integer
+  wc = Math.floor(wc);
+  
+  //If chill is greater than temp, return the temp
+  wc = (wc > temp) ? temp : wc;
+  
+  //Display the wiindchill
+  console.log(wc);
+  console.log(speed);
+  console.log(temp);
+  
+  // feelTemp.innerHTML = wc;
+  
+  return wc;
+}
