b2ea21a7e6f5222150acc44d5b4ce14c4943aece
charletmbizi <90606039+charletmbizi@users.noreply.github.com>
2021-11-26 10:02:49 +0200

    Create lesson11.js

---
+++ lesson-11/js/lesson11.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".ham");  
+ const navsub = document.querySelector(".nav-sub");  
+ hamburger.addEventListener('click', () => {  
+  hamburger.classList.toggle("change")  
+  navsub.classList.toggle("nav-change")  
+ });
+
+var day = new Date().getDay(); // getDay returns 0 to 6; 0 = sun, 1 = mon and so on.
+ if (day < 6) {    
+ document.getElementById("hidden").style.display = "none";
+    } 
+else {
+document.getElementById("hidden").style.display = "block";       
+}
+document.getElementById("current-date").innerHTML = new Date().toDateString();
+document.getElementById("year").innerHTML = new Date().getFullYear();
